c9ee9a3b0fe90154ce27ac6c9a777636f630f3d8
oguramidori <nnieeeee@gmail.com>
2020-04-26 18:09:03 +0900

    質問４の訂正と質問６の回答

--- task.rb
+++ task.rb
@@ -23,14 +23,15 @@ def q3
 
   # 以下に回答を記載
   p numbers.count(3)
-  
 end
 
 def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-  sports.delete(nil)
+  
+  # 訂正前: sports.delete(nil)
+  sports.compact!
   # 以下は変更しないで下さい
   p sports
 end
@@ -46,9 +47,11 @@ end
 
 def q6
   numbers1 = [1, 2, 3, 4, 5]
+  
 
   # 以下に回答を記載
-
+  numbers2 = numbers1.map { |n| n * 10 }
+  p numbers2
 end
 
 def q7
