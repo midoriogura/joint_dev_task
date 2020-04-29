07c1764c7b25008f613526e01ce8e12b47b3dffd
oguramidori <nnieeeee@gmail.com>
2020-04-29 11:54:46 +0900

    Q7,８を回答しました

--- task.rb
+++ task.rb
@@ -29,7 +29,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-  
+
   # 訂正前: sports.delete(nil)
   sports.compact!
   # 以下は変更しないで下さい
@@ -50,7 +50,7 @@ def q6
   
 
   # 以下に回答を記載
-  numbers2 = numbers1.map { |n| n * 10 }
+  numbers2 = numbers1.map {|n| n * 10 }
   p numbers2
 end
 
@@ -58,7 +58,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+  array.map!(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
@@ -67,6 +67,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
 
   # 以下は変更しないで下さい
   p programming_languages
