a2f3c3d295bf14c52e2cdb7a8c4ed8320c6b2e6e
oguramidori <nnieeeee@gmail.com>
2020-05-02 21:53:37 +0900

    Q17の回答をしました

--- task.rb
+++ task.rb
@@ -148,9 +148,19 @@ end
 
 class UserQ17
   # 以下に回答を記載
-
-end
-
+  def initialize(**params)
+    @name = params[:name]
+    @age = params[:age]
+    @gender = params[:gender]
+  end  
+  def info
+    puts <<~TEXT
+    "名前：#{@name}"
+    "年齢：#{@age}"
+    "性別：#{@gender}"
+    TEXT
+  end     
+end      
 def q17
   # ここは変更しないで下さい（ユーザー情報は変更していただいてOKです）
   user1 = UserQ17.new(name: "神里", age: 32, gender: "男")
@@ -163,7 +173,7 @@ end
 
 class UserQ18
   # 以下に回答を記載
-
+  
 end
 
 def q18
